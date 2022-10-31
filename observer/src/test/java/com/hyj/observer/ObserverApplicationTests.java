60e07071390e11cf3d265f4087e371b398c70c3f
yjhuang <478619644@qq.com>
2022-10-31 15:13:06 +0800

    提交新的内容

---
+++ observer/src/test/java/com/hyj/observer/ObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyj.observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
